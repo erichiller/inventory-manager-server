06b1f166f5cdda373353889b693f77d91c2a35a5
Eric D. Hiller <eric@hiller.pro>
2020-01-04 06:31:55 -0700

    descriptions for server GraphQL; fix vulnerability

--- TODO.md
+++ TODO.md
@@ -51,11 +51,12 @@
 
 * [x] Ensure `printRaster` now works
 * [x] Pass data from client to server/api for `printRaster`
-* [ ] Ensure multiple labels (Chain Printing) print properly.
-* [ ] Ensure setting label length statically will also work. (Or calculate & display in UI)
-* [ ] Figure out how to use the last 6 pixels on 12mm tape  
+* [x] Ensure multiple labels (Chain Printing) print properly.
+* [x] Figure out how to use the last 6 pixels on 12mm tape  
       150 is supported max. 48 * 3 = 144 is all I can get currently without printing a second page;
+* [ ] Ensure setting label length statically will also work. (Or calculate & display in UI)
 * [ ] `canvasToBuffer` should also send along the number of pixels, otherwise if the last byte only has a single bit of importance, a whole extra byte is added.
-* [ ] `printRaster` should set the tape width to the actual tape width
+* [ ] `printRaster()` should set the tape width to the actual tape width
+* [ ] `<LabelComponent>` should set the text color and background color to the actual color from the `PrinterStatus` GraphQL Query
 * [ ] Complete `PrinterStatus` in `epson.ts` and related GraphQL query
   * [ ] flesh out the ENUMs used by `epson.ts` and populate GraphQL
--- server/src/schema/schema_ast.ts
+++ server/src/schema/schema_ast.ts
@@ -7,57 +7,80 @@ import { makeExecutableSchema } from 'apollo-server';
  * GraphQL Schema
  **/
 const typeDefs = gql`
-  scalar Upload
+    scalar Upload
 
-  type File {
-    # __typename: String
-    name: String!
-    path: String!
-    type: String!
-    base64: String!
-  }
+    type File {
+        # __typename: String
+        name: String!
+        path: String!
+        type: String!
+        base64: String!
+    }
 
-  type Query {
-    files: [File]
-    PrinterStatus: PrinterStatus
-  }
+    type Query {
+        files: [File]
+        """
+        Retrieve Printer and Label status and properties
+        """
+        PrinterStatus: PrinterStatus!
+    }
 
-  type LabelCharacteristic {
-    widthMillimeters: Int
-    pinsLeft: Int
-    pinsPrint: Int
-    pinsRight: Int
-  }
 
-  type PrinterLabelStatus {
+    """
+    Current Label size and that size's properties.
+    """
+    type LabelCharacteristic {
+        widthMillimeters: Int!
+        pinsLeft: Int!
+        pinsPrint: Int!
+        pinsRight: Int!
+    }
+
+    """
+    Label characteristics and properties
+    """
+    type PrinterLabelStatus {
         # mediaType: MEDIA_TYPE;
         # mediaWidth: MEDIA_WIDTH;
         labelCharacteristic: LabelCharacteristic
         # /** NodeJS buffer */
         # bytes: Buffer;
-  }
+    }
 
-  type PrinterStatus {
-    labelType: String!
-    uptime: Int
-    model: String
-    firmwareVersion: Float
-    heightInch: Float
-    heightMillimeter: Float
-    labelStatus: PrinterLabelStatus
-  }
+    """
+    Printer and label status and properies
+    """
+    type PrinterStatus {
+        labelType: String!
+        uptime: Int!
+        model: String!
+        firmwareVersion: Float!
+        heightInch: Float
+        heightMillimeter: Float
+        labelStatus: PrinterLabelStatus!
+    }
   
-  scalar uint8
+    """
+    unsigned byte
+    0 - 255
+    """
+    scalar uint8
 
-  type LabelMonochromeBuffer {
-    # __typename: String
-    imageBuffer: [[[uint8]]]
-  }
+    """
+    Image Buffer / Raster data arranged as [page][column][pixels] of uint8 to the printer
+    """
+    type LabelMonochromeBuffer {
+        # __typename: String
+        imageBuffer: [[[uint8]]]!
+    }
 
-  type Mutation {
-    uploadFiles(files: [Upload]!): [File]!
-    putLabelMonochromeBuffer(imageBuffer: [[[uint8]]]!): [LabelMonochromeBuffer]
-  }
+    type Mutation {
+        uploadFiles(files: [Upload]!): [File]!
+        """
+        Send a label to be printed
+        """
+        putLabelMonochromeBuffer(imageBuffer: [[[uint8]]]!): [LabelMonochromeBuffer]
+    }
 
 `;
 
