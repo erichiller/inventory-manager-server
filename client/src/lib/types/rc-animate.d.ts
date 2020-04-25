2a227abd1476b961a225f14a49dfb2c2be8500fd
Eric D. Hiller <eric@hiller.pro>
2020-04-25 08:10:34 -0600

    add rc-animate type defs

---
+++ client/src/lib/types/rc-animate.d.ts
@@ -0,0 +1,20 @@
+declare module 'rc-animate' {
+    export interface Props {
+        component: string | React.ReactElement<any>;
+        componentProps: object;
+        animation: object;
+        transitionName: string | object;
+        transitionEnter: boolean;
+        transitionAppear: boolean;
+        exclusive: boolean;
+        transitionLeave: boolean;
+        onEnd: ( key: string, exists: boolean ) => void;
+        onEnter: ( key: string ) => void;
+        onLeave: ( key: string ) => void;
+        onAppear: ( key: string ) => void;
+        showProp: string;
+        children: React.ReactChild;
+    }
+
+    export default class Animate extends React.Component<Partial<Props>> { }
+}
