a3a5ff2952258fde65becbc4c972df2372b4bea0
Eric D. Hiller <eric@hiller.pro>
2020-06-03 07:07:24 -0600

    fixed selected SubMenu highlight in MainMenu

--- client/src/components/shared/Menu.tsx
+++ client/src/components/shared/Menu.tsx
@@ -7,12 +7,14 @@ import { PrintListButton } from "../print/PrintListButton";
 
 export const MainMenu: React.FC<{}> = ( props ) => {
     const location = useLocation();
-    return <Menu mode="horizontal" selectedKeys={[ location.pathname ]} >
+    return <Menu className="MainMenu" mode="horizontal" selectedKeys={[ location.pathname ]} >
         <Menu.Item key="/"><Link to="/"><DashboardOutlined />Summary</Link></Menu.Item>
         <Menu.Item key="/item"><Link to="/item"><ContainerOutlined />Items</Link></Menu.Item>
         <Menu.Item key="/network"><Link to="/network"><ShareAltOutlined />Network</Link></Menu.Item>
         <Menu.Item key="/label"><Link to="/label"><TagOutlined />Labels</Link></Menu.Item>
+        {/* <Menu.SubMenu key="/order" title={<span><ShoppingCartOutlined />Orders</span>} > */}
         <Menu.SubMenu key="/order" title={<Link to="/order"><ShoppingCartOutlined />Orders</Link>} >
+            <Menu.Item key="/order"><Link to="/order"><PlusCircleOutlined />List</Link></Menu.Item>
             <Menu.Item key="/order/_/add"><Link to="/order/_/add"><PlusCircleOutlined />Add Order</Link></Menu.Item>
         </Menu.SubMenu>
         <PrintListButton key="PrintListButton" style={{ float: 'right' }} />
