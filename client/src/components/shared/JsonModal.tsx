e93fe03a0f274c0ede87fdca81bcc6e85b80b78c
Eric D. Hiller <eric@hiller.pro>
2020-06-23 18:57:00 -0600

    styling, utility funcs

--- client/src/components/shared/JsonModal.tsx
+++ client/src/components/shared/JsonModal.tsx
@@ -5,17 +5,27 @@ import { Button, Modal } from "antd";
 
 interface JsonModalProps {
     json: Object | string;
+    onChange?: (json: Object) => void;
+    onCommit?: (json: Object) => void;
     visibilityHandler: ( modal: React.ReactElement ) => void;
 }
 
 export const JsonModal: React.FC<JsonModalProps> = ( props ) => {
-    const { visibilityHandler } = props;
+    const { visibilityHandler, onCommit } = props;
     const json = typeof props.json === "string" ? JSON.stringify( props.json ) : props.json;
+
+    const onChange = (json: Object) => {
+        // run something that prepares for `onCommit`
+        if ( props.onChange ){
+            props.onChange(json);
+        }
+    }
 
     console.log({cls: "JsonModal", evt: "ModalInit", json});
     return <Modal
         title="JSON"
         visible={true}
+        className="JsonModal"
         // onOk={() => visibilityHandler(false)}
         // onCancel={this.handleCancel}
         footer={[
@@ -24,7 +34,9 @@ export const JsonModal: React.FC<JsonModalProps> = ( props ) => {
             </Button>,
         ]}
     >
-        <JsonEditor json={json} />
+        <JsonEditor json={json}
+            {...onChange ?? {}}
+         />
     </Modal>;
 };
 
@@ -48,21 +60,28 @@ type JsonEditorProps = Union<{
 } | {
     json?: object;
     text: string;
-// } | {
-//     vendor?: null;
-//     vendorId?: null;
-}, JSONEditorOptions >;
+}, {
+    className?: string
+},
+JSONEditorOptions >;
 
 
+// TODO: JsonEditorModal Templates ; create templates for common data entry. Array of templates that will appear in the context menu, Each template is a json object precreated that can be added as a object value to any node in your document
 const JsonEditor: React.FC<JsonEditorProps> = ( props ) => {
-    const { json, text, mode, schema, schemaRefs } = props;
+    const { json, text, mode, schema, schemaRefs, className } = props;
 
-
+    /**
+     * More information about [jsoneditor](
+     * https://github.com/josdejong/jsoneditor)
+     * 
+     * * [API docs](https://github.com/josdejong/jsoneditor/blob/develop/docs/api.md)
+     */
     let JsonEditorInstance: JSONEditor;
     let options: Omit<JsonEditorProps, 'json' | 'text'>;
 
-    const containerRef = useRef();
+    const containerRef = useRef<HTMLDivElement>();
 
+    // run this once
     useEffect( () => {
         // copy all properties into options for the editor
         // (except the properties for the JSONEditorReact component itself)
@@ -86,7 +105,6 @@ const JsonEditor: React.FC<JsonEditorProps> = ( props ) => {
         return function cleanup () {
             JsonEditorInstance.destroy();
         };
-
     } );
 
     useEffect( () => {
@@ -109,10 +127,9 @@ const JsonEditor: React.FC<JsonEditorProps> = ( props ) => {
             options.schemaRefs = deepCopy( schemaRefs );
             JsonEditorInstance.setSchema( schema, schemaRefs );
         }
-
     }, [ json, text, mode, schema, schemaRefs ] );
 
     return (
-        <div className="jsoneditor-react-container" ref={containerRef} />
+        <div className={`jsoneditor-react-container ${className??''}`} ref={containerRef} />
     );
 };
